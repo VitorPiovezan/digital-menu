0b149b0e53b61e7cbf392bc062f1311496adcc37
VitorPiovezan <vitor.piovezan@hotmail.com.br>
2022-07-25 04:50:56 -0300

    Adicioando dependencias de estilo

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Cardapio from './pages/cardapio';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <div>Digital Menu</div>
+    <Cardapio />
   </React.StrictMode>
 );
