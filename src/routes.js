174f35b61be5ed26af35eaf5e84811ac42f1c232
VitorPiovezan <vitor.piovezan@hotmail.com.br>
2022-07-27 18:58:40 -0300

    Criado rotas de pratos e estilização - uso do useParams e useLocation"

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sobre from 'pages/sobre';
 import Footer from 'components/footer';
 import NotFound from 'pages/notFound';
+import Prato from 'pages/prato';
 
 export default function AppRouter() {
   return (
@@ -17,6 +18,7 @@ export default function AppRouter() {
             <Route index element={<Inicio />} />
             <Route path="cardapio" element={<Cardapio />} />
             <Route path="sobre" element={<Sobre />} />
+            <Route path="prato/:id" element={<Prato />} />
           </Route>
           <Route path="*" element={<NotFound />} />
         </Routes>
