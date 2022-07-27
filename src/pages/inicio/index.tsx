73fab75e574e4b84982f2d9486a6b740044a47de
VitorPiovezan <vitor.piovezan@hotmail.com.br>
2022-07-26 23:48:17 -0300

    Desenvolvido mais paginas e adicionado rotas 404
    - Nesse commit foi ajustado o menu fixo para funcionar todas as rotas
    - Criado uma pagina 404 com opção de voltar para tela anterior
    e foi ajustado os estilos de cada component
    - Criado footer também

--- src/pages/inicio/index.tsx
+++ src/pages/inicio/index.tsx
@@ -1,6 +1,7 @@
 import cardapio from 'data/cardapio.json';
 import styles from './Inicio.module.scss';
-
+import stylesTema from 'styles/Tema.module.scss';
+import nossaCasa from 'assets/nossa_casa.png';
 export default function Inicio() {
   let pratosRecomendados = [...cardapio];
   pratosRecomendados = pratosRecomendados
@@ -8,7 +9,7 @@ export default function Inicio() {
     .splice(0, 3);
   return (
     <section>
-      <h3 className={styles.titulo}>Recomendações da cozinha</h3>
+      <h3 className={stylesTema.titulo}>Recomendações da cozinha</h3>
       <div className={styles.recomendados}>
         {pratosRecomendados.map(item => (
           <div key={item.id} className={styles.recomendado}>
@@ -18,6 +19,14 @@ export default function Inicio() {
             <button className={styles.recomendado__botao}>Ver mais</button>
           </div>
         ))}
+      </div>
+      <h3 className={stylesTema.titulo}>Nossa casa</h3>
+      <div className={styles.nossaCasa}>
+        <img src={nossaCasa} alt="Casa Aluroni" />
+        <div className={styles.nossaCasa_endereco}>
+          Rua Julio Lopes, 2312 <br />
+          <br />
+        </div>
       </div>
     </section>
   );
--- src/routes.js
+++ src/routes.js
@@ -3,19 +3,24 @@ import Menu from 'components/menu';
 import Cardapio from 'pages/cardapio';
 import Inicio from 'pages/inicio';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Sobre from 'pages/sobre';
+import Footer from 'components/footer';
+import NotFound from 'pages/notFound';
 
 export default function AppRouter() {
   return (
-    <main>
+    <main className="container">
       <Router>
         <Menu />
-
         <Routes>
           <Route path="/" element={<PaginaPadrao />}>
             <Route index element={<Inicio />} />
             <Route path="cardapio" element={<Cardapio />} />
+            <Route path="sobre" element={<Sobre />} />
           </Route>
+          <Route path="*" element={<NotFound />} />
         </Routes>
+        <Footer />
       </Router>
     </main>
   );
