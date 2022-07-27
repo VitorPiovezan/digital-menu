e001e3c2af5810fedd33b8193a5f7dfaeee24543
VitorPiovezan <vitor.piovezan@hotmail.com.br>
2022-07-26 22:06:45 -0300

    Criado arquivo de rotas e refatorado codigo componentizando cabeçalho

---
+++ src/routes.js
@@ -0,0 +1,22 @@
+import PaginaPadrao from 'components/paginaPadrao';
+import Menu from 'components/menu';
+import Cardapio from 'pages/cardapio';
+import Inicio from 'pages/inicio';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+export default function AppRouter() {
+  return (
+    <main>
+      <Router>
+        <Menu />
+
+        <Routes>
+          <Route path="/" element={<PaginaPadrao />}>
+            <Route index element={<Inicio />} />
+            <Route path="cardapio" element={<Cardapio />} />
+          </Route>
+        </Routes>
+      </Router>
+    </main>
+  );
+}
